b7a30e8913848a13ae10eb15a6721acb0ad0e441
Kunal Ladhani <k.ladhani1@gmail.com>
2023-09-22 14:34:25 +0530

    Time: 4 ms (87.52%), Space: 44 MB (8.89%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,19 +1,35 @@
 class Solution {
-    public int[] smallerNumbersThanCurrent(int[] nums) {
-//         int n = nums.length, c = 0;
-//         int[] ans = new int[n];
+    public int[] smallerNumbersThanCurrent(int[] arr) {
+        int n = arr.length;
+//         int[] temp = new int[n];
         
-//         // TC = O(N) SC = O(1) excluding the space for answer
-//         Arrays.sort(nums);  
-        
+//         //TC = O(N), SC = O(N)
 //         for(int i=0; i<n; i++) {
-                    
+//             temp[i] = arr[i];
 //         }
+        int[] temp = arr.clone();
         
-//         return ans;
-//     }
+        Arrays.sort(temp);  // O(NlogN)
+        
+        //TC = O(N), SC = O(N)
+        Map<Integer,Integer> smaller = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            // if(smaller.get(temp[i]) == null) {
+            //     smaller.put(temp[i],i);    
+            // }
+            smaller.putIfAbsent(temp[i],i);
+        }
+
+        // TC = O(N)        
+        for(int i=0; i<n; i++) {
+            temp[i] = smaller.get(arr[i]);
+        }
+         
+        // TC = O(NlogN)
+        return temp;
+    }
     
-//     public int[] trivialSolution(int[] nums) {
+    public int[] trivialSolution(int[] nums) {
         int n = nums.length, c = 0;
         int[] ans = new int[n];
         
