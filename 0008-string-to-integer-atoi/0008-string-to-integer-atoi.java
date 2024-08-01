676e308f05f19df163864dfedc58576984c8af1f
Kunal Ladhani <k.ladhani1@gmail.com>
2024-08-01 22:28:07 +0530

    Time: 1 ms (100.00%), Space: 42.3 MB (60.75%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.java
@@ -26,16 +26,21 @@ class Solution {
         
         boolean isNeg = (s.charAt(startIdx) == '-');
         
+        // if + or - is there then skip that char
         if(isNeg || (s.charAt(startIdx) == '+')) {
             startIdx++;
         }
         
+        // iterate till next non num char or end whichever comes first.
         while(startIdx < len && isNum(s.charAt(startIdx))) {
             int n = toNum(s.charAt(startIdx));
             
             // check for overflow
-            if(Integer.MAX_VALUE / 10 < ans || Integer.MAX_VALUE / 10 == ans && Integer.MAX_VALUE % 10 < n)
+            if( Integer.MAX_VALUE / 10 < ans || // ans*10 + n will cause overflow in this case for any n
+                (Integer.MAX_VALUE / 10 == ans && Integer.MAX_VALUE % 10 < n) // ans*10 will cause overflow if n > last dig of int_max
+              ) {
                 return isNeg ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            }
             
             ans = (ans*10) + n;
             startIdx++;
