b679f91357a1f63706584fbd47316bbf40152a70
Kunal Ladhani <k.ladhani1@gmail.com>
2024-09-05 06:28:41 +0530

    Time: 2 ms (90.87%), Space: 46.7 MB (88.30%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -1,23 +1,15 @@
 class Solution {
-    public int[] twoSum(int[] arr, int k) {
+    public int[] twoSum(int[] numbers, int target) {
+        return twoSumSorted(numbers,target);
+    }
+    
+    // TC = O(N), SC = O(N)
+    public int[] twoSumUnsorted(int[] arr, int k) {
         int[] ans = new int[2];
         int n = arr.length;
         if (n <= 1) return ans;
         
         Map<Integer,Integer> map = new HashMap<>();
-        // int l = 0, h = n-1;
-        // while(l < h) {
-        //     if (arr[l] + arr[h] == k) {
-        //         ans[0] = l+1;
-        //         ans[1] = h+1;
-        //         return ans;
-        //     } else if (arr[l] + arr[h] > k) {
-        //         h--;
-        //     } else {
-        //         l++;
-        //     }
-        // }
-        
         for(int i=0; i<n; i++) {
             if(map.containsKey(k - arr[i])) {
                 ans[0] = map.get(k - arr[i]) + 1;
@@ -28,4 +20,27 @@ class Solution {
         }
         return ans;
     }
+    
+    // TC = O(N), SC = O(1)
+    public int[] twoSumSorted(int[] arr, int k) {
+        int[] ans = new int[2];
+        int n = arr.length;
+        if (n <= 1) return ans;
+    
+        int l = 0, h = n-1;
+        while(l < h) {
+            if (arr[l] + arr[h] == k) {
+                ans[0] = l+1;
+                ans[1] = h+1;
+                return ans;
+            } else if (arr[l] + arr[h] > k) {
+                h--;
+            } else {
+                l++;
+            }
+        }
+        return ans;
+    }
+    
+        
 }
