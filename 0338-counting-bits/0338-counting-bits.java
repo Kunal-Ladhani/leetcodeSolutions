1ac4e4695b203d97b9b27a9c8c6c4aee2a1e8faf
Kunal Ladhani <k.ladhani1@gmail.com>
2025-06-02 20:42:17 +0530

    Time: 2 ms (94.59%), Space: 46.8 MB (34.51%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,33 +1,18 @@
 class Solution {
-    private int countSetBits(int n) {
-        if(n == 0) {
-            return 0;
+    private static int countSetBits(int n) {
+        int ans = 0;
+        while (n > 0) {
+            n = n & n-1;
+            ans++;
         }
-        
-        int carry = 0, count = 0;
-        
-        while(n > 0) {
-            if(carry == 1) {
-                count++;
-            }
-            carry = n%2;
-            n = n/2;
-        }
-        
-        if(carry == 1) {
-            count++;
-        }
-        
-        return count;
+        return ans;
     }
-    
+
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
-        
-        for(int i=0; i<=n; ++i) {
+        for (int i=0; i<=n; i++) {
             ans[i] = countSetBits(i);
         }
-        
         return ans;
     }
 }
