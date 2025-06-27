4e30d9c7f2e9c539d3a3f9740aedf0d742ba7cc2
Kunal Ladhani <k.ladhani1@gmail.com>
2025-06-27 17:46:23 +0530

    Time: 0 ms (100%), Space: 42.7 MB (31.88%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,28 +1,34 @@
 class Solution {
+    public double naive(double x, int n) {
+        if (n == 0) return 1;
+        if (n == 1) return x;
+
+        if (n<0)
+            return (double)(1/myPow(x,Math.abs(n)));
+
+        double ans = myPow(x, n/2);
+        if (n%2 == 0)
+            return ans * ans;
+        else
+            return x * ans * ans;
+    }
+
     public double myPow(double x, int n) {
-        if(x == 0) return 0.0;
-        if(n == 0) return 1.0;
-        if (n == Integer.MAX_VALUE) return (x == 1) ? 1 : (x == -1) ? -1 : 0;
-        if (n == Integer.MIN_VALUE) return (x == 1 || x == -1) ? 1 : 0;
-        
-        if(n == 1) return x;
-        else if (n > 1) {
-            if(n%2 == 0) {
-                double k = myPow(x,n/2);
-                return k * k;
-            } else {
-                double p = myPow(x,(n-1)/2);
-                return p * p * x;
-            }
-        } else {
-            n = n * -1;
-            if(n%2 == 0) {
-                double k = myPow(x,n/2);
-                return 1/(k * k);
-            } else {
-                double p = myPow(x,(n-1)/2);
-                return 1/(p * p * x);
-            }
+        long N = n;
+        if (N < 0) {
+            x = 1 / x;
+            N = -N;
         }
+
+        return fastPow(x, N);
+    }
+
+    private double fastPow(double x, long n) {
+        if (n == 0) return 1;
+        double half = fastPow(x, n / 2);
+        if (n % 2 == 0)
+            return half * half;
+        else
+            return x * half * half;
     }
 }
