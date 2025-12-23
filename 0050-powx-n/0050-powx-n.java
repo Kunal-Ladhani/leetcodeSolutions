7dd3e97d1283a72fce833938ac87bf15c9de4dc4
Kunal Ladhani <k.ladhani1@gmail.com>
2025-12-23 22:19:36 +0530

    Time: 0 ms (100%), Space: 47.4 MB (77.53%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,34 +1,41 @@
 class Solution {
-    public double naive(double x, int n) {
-        if (n == 0) return 1;
-        if (n == 1) return x;
 
-        if (n<0)
-            return (double)(1/myPow(x,Math.abs(n)));
+    /**
+        pow(x,n) = x^n = 
+        
+        we have 2 options here -
 
-        double ans = myPow(x, n/2);
-        if (n%2 == 0)
-            return ans * ans;
-        else
-            return x * ans * ans;
+        n == odd 
+        => n = 2k+1
+        pow(x,n) = pow(x,n/2) * pow(x,n/2) * x 
+
+        n == even 
+        => n = 2k
+        pow(x,n) = pow(x,n/2) * pow(x,n/2)
+    */
+    public double myPow(double x, int n) {
+        return binaryExp(x, (long) n);
     }
 
-    public double myPow(double x, int n) {
-        long N = n;
-        if (N < 0) {
-            x = 1 / x;
-            N = -N;
+    private static double binaryExp(double x, long n) {
+        if (n == 0)
+            return 1.0d;
+        
+        if (n == 1)
+            return x;
+        
+        if (n < 0) {
+            x = (double) 1/x;
+            n = -1 * n;
         }
 
-        return fastPow(x, N);
-    }
+        double half;
+        if (n%2 == 0) {
+            half = binaryExp(x, n/2);            
+            return half * half;            
+        }
 
-    private double fastPow(double x, long n) {
-        if (n == 0) return 1;
-        double half = fastPow(x, n / 2);
-        if (n % 2 == 0)
-            return half * half;
-        else
-            return x * half * half;
+        half = binaryExp(x, (n-1)/2);   
+        return half * half * x;
     }
 }
