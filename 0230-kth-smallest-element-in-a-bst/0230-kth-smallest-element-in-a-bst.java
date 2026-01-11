9aa491fc71b4825ef182ba355d74e558f6e00681
Kunal Ladhani <k.ladhani1@gmail.com>
2026-01-11 20:23:37 +0530

    Time: 0 ms (100%), Space: 46.9 MB (35.18%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -15,15 +15,44 @@
  */
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        return naive(root, k);    
+        // return naive(root, k);
+        return optimal(root, k);
     }
+
+    // --------- optimal ----------
+
+    private static int optimal(TreeNode root, int k) {
+        int[] ans = new int[2];
+        // ans[0] = counter
+        // ans[1] = value
+
+        solver(root, k, ans);
+        return ans[1];
+    }
+
+    private static void solver(TreeNode root, int k, int[] ans) {
+        if (root == null)
+            return;
+        
+        solver(root.left, k, ans);
+
+        ans[0] += 1;
+        if (ans[0] == k) {
+            ans[1] = root.val;
+            return;
+        }
+        
+        solver(root.right, k, ans);
+    }
+
+    // --------- naive ----------
 
     private static int naive(TreeNode root, int k) {
         List<Integer> arr = new ArrayList<>();
         inorder(root, arr);
         return arr.get(k-1);
     }
-    
+
     private static void inorder(TreeNode root, List<Integer> arr) {
         if (root == null)
             return;
