4b282e56921dabf7f9bbb7806dc10279904260fd
Kunal Ladhani <k.ladhani1@gmail.com>
2026-06-11 05:01:04 +0530

    Time: 2 ms (99.69%), Space: 45.3 MB (24.1%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,20 +1,43 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Set<List<Integer>> ans = new HashSet<>();
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(nums);
-        helper(nums, 0, new ArrayList<>(), ans);
-        return new ArrayList<>(ans);
+        // recUtil(nums, ans, new ArrayList<>(), 0);
+        backtrack(nums, ans, new ArrayList<>(), 0);
+        return ans;    
     }
-
-    private static void helper(int[] nums, int ptr, List<Integer> temp, Set<List<Integer>> ans) {
-        if (ptr == nums.length) {
+    // select and not select
+    private static void recUtil(int[] nums, List<List<Integer>> ans, List<Integer> temp, int idx) {
+        if (idx == nums.length) {
             ans.add(new ArrayList<>(temp));
             return;
         }
+        
+        temp.add(nums[idx]);
+        recUtil(nums, ans, temp, idx+1);    // select
 
-        helper(nums, ptr + 1, temp, ans);   // not select
-        temp.add(nums[ptr]);
-        helper(nums, ptr + 1, temp, ans);   // not select
-        temp.remove(temp.size()-1);     // backtrack        
+        temp.remove(temp.size()-1); // backtrack
+
+        while (idx+1 < nums.length && nums[idx] == nums[idx+1])
+            idx++;
+
+        recUtil(nums, ans, temp, idx+1);    // not select
     }
+
+    // backtracking soln
+    private static void backtrack(int[] nums, List<List<Integer>> ans, List<Integer> temp, int idx) {
+        
+        ans.add(new ArrayList<>(temp));
+        
+        for (int i = idx; i < nums.length; ++i) {
+            if (i > idx && nums[i-1] == nums[i])
+                continue;
+            
+            temp.add(nums[i]);
+            backtrack(nums, ans, temp, i+1);  // select
+            temp.remove(temp.size()-1); // backtrack
+        }
+    }
+
+    
 }
